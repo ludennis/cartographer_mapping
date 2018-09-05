95e5ad5eafc1cc573db0354acb9cbc344ebbcd7f
455936 <Dennis.Lu@itri.org.tw>
2018-09-05 09:56:49 +0800

    initial commit for installation script

---
+++ install_cartographer.sh
@@ -0,0 +1,17 @@
+rm -rf build_isolated/ devel_isolated/ install_isolated/
+
+sudo apt-get update
+sudo apt-get install -y python-wstool python-rosdep ninja-build
+
+wstool init src
+
+wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
+wstool update -t src
+
+src/cartographer/scripts/install_proto3.sh
+
+sudo rosdep init
+rosdep update
+rosdep install --from-paths src --ignore-src --rosdistro=${ROS_DISTRO} -y
+
+catkin_make_isolated --install --use-ninja
