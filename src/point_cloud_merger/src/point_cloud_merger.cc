5615a1257d7df43bbfe0cd44e212bf89a1c700d3
455936 <Dennis.Lu@itri.org.tw>
2018-09-21 15:40:10 +0800

    point_cloud_merger: commandline functionality

---
+++ src/point_cloud_merger/src/point_cloud_merger.cc
@@ -0,0 +1,50 @@
+/*
+    Package to merge multiple point clouds (.pcd files) into one
+*/
+
+#include <iostream>
+#include <pcl/io/pcd_io.h>
+
+int main(int argc, char* argv[])
+{
+    if (argc < 3)
+    {
+        printf("Usage: point_cloud_merger [source pcd filename] [target pcd filename]\n");
+        return -1;
+    }
+
+    // declare point cloud pointers
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_source (new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud_target (new pcl::PointCloud<pcl::PointXYZ>);
+
+    //load point clouds (.pcd files) from commandline arguments
+    printf("Opening pcd file: '%s' ... ", argv[1]);
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[1], *cloud_source) == -1)
+    {
+        PCL_ERROR ("Error: couldn't read file %s\n", argv[1]);
+        return -1;
+    }
+    else
+    {
+        printf("Success! Loaded %i data points.\n", cloud_source->width * cloud_source->height);
+    }
+
+    printf("Opening pcd file: '%s' ... ", argv[2]);
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (argv[2], *cloud_target) == -1)
+    {
+        PCL_ERROR ("Error: couldn't read file %s\n", argv[2]);
+        return -1;
+    }
+    else
+    {
+        printf("Success! Loaded %i data points.\n", cloud_target->width * cloud_target->height);
+    }
+
+    // run ICP over point clouds
+
+    // apply transformations to a combined point clouds
+
+    // save in file
+
+    return 0;
+}
