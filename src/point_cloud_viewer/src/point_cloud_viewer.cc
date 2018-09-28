c6897e892aa017f96683a83b1d01c2b2d457ef62
455936 <Dennis.Lu@itri.org.tw>
2018-09-28 16:14:28 +0800

    point_cloud_viewer: load pcd files from commandline arguments

--- src/point_cloud_viewer/src/point_cloud_viewer.cc
+++ src/point_cloud_viewer/src/point_cloud_viewer.cc
@@ -9,11 +9,24 @@
 #include <iostream>
 #include <vector>
 
+#include <pcl/io/pcd_io.h>
+
+// global variables
+std::vector<pcl::PointCloud<pcl::PointXYZ>::Ptr> cloud_pointers;
+
 int main(int argc, char** argv)
 {
 	for (int i = 0; i < argc; ++i)
 	{
 		printf("argv[%i]: %s\n", i, argv[i]);
+	}
+
+	for (int i = 1; i < argc; ++i)
+	{
+		// load point cloud file and add to vector
+		pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+		pcl::io::loadPCDFile<pcl::PointXYZ>(argv[i], *cloud);
+		cloud_pointers.push_back(cloud);
 	}
 
 	return 0;
