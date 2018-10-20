e087760899a3f84b26b2d4a2a7429d6bc28934d1
455936 <Dennis.Lu@itri.org.tw>
2018-10-20 18:41:04 +0800

    velodyne_extractor: read bag file containing velodyne, gps, and imu data

--- src/velodyne_extractor/src/velodyne_extractor.cpp
+++ src/velodyne_extractor/src/velodyne_extractor.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/NavSatFix.h>
+#include <sensor_msgs/Imu.h>
+
+#include <pcl/common/common.h>
+#include <pcl_conversions/pcl_conversions.h>
+
+#include <boost/foreach.hpp>
+#define foreach BOOST_FOREACH
+
+typedef pcl::PointXYZI PointT;
+typedef pcl::PointCloud<PointT> PointCloudT;
+typedef pcl::PointCloud<PointT>::Ptr PointCloudTPtr;
+
 int main (int argc, char** argv)
 {
     if (argc < 6)
@@ -16,6 +33,34 @@ int main (int argc, char** argv)
     float intensity_low_pass(atof(argv[4]));
     float intensity_high_pass(atof(argv[5]));
     strcpy(read_bag_filename, argv[6]);
+
+    rosbag::Bag read_bag;
+    read_bag.open(read_bag_filename, rosbag::bagmode::Read);
+    std::vector<std::string> topics;
+    topics.push_back(std::string("/velodyne_points"));
+    topics.push_back(std::string("/fix"));
+    topics.push_back(std::string("/imu/data"));
+
+    rosbag::View view(read_bag, rosbag::TopicQuery(topics));
+
+    foreach (rosbag::MessageInstance const m, view)
+    {
+        ros::Time msg_time = m.getTime();
+        sensor_msgs::PointCloud2::ConstPtr pc2_msg =
+            m.instantiate<sensor_msgs::PointCloud2>();
+        sensor_msgs::NavSatFix::ConstPtr fix_msg =
+            m.instantiate<sensor_msgs::NavSatFix>();
+        sensor_msgs::Imu::ConstPtr imu_msg =
+            m.instantiate<sensor_msgs::Imu>();
+
+        if ( pc2_msg != NULL)
+        {
+            PointCloudTPtr cloud_ptr(new PointCloudT);
+            pcl::PCLPointCloud2 pcl_pc2;
+            pcl_conversions::toPCL(*pc2_msg, pcl_pc2);
+            pcl::fromPCLPointCloud2(pcl_pc2, *cloud_ptr);
+        }
+    }
 
     return 0;
 }
