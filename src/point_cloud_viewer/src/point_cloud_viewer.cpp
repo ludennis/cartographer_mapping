4d8d9ad99cda2d127738a55293cc759a42e36db9
455936 <Dennis.Lu@itri.org.tw>
2018-11-05 21:22:37 +0800

    point_cloud_viewer: fixed indentation by avoiding using tabs

--- src/point_cloud_viewer/src/point_cloud_viewer.cpp
+++ src/point_cloud_viewer/src/point_cloud_viewer.cpp
@@ -19,43 +19,43 @@ PointCloudTPtr map_cloud_ptr (new PointCloudT);
 
 void pointPickingOccurred (const pcl::visualization::PointPickingEvent &event)
 {
-	PointT point_clicked (map_cloud_ptr->points[event.getPointIndex()]);
-	ROS_INFO("Point index %i at (x=%f, y=%f, z=%f, intensity=%f) was clicked.\n",
-		event.getPointIndex(), point_clicked.x,	point_clicked.y, point_clicked.z,
-		point_clicked.intensity);
+    PointT point_clicked (map_cloud_ptr->points[event.getPointIndex()]);
+    ROS_INFO("Point index %i at (x=%f, y=%f, z=%f, intensity=%f) was clicked.\n",
+        event.getPointIndex(), point_clicked.x,	point_clicked.y, point_clicked.z,
+        point_clicked.intensity);
 }
 
 int main(int argc, char** argv)
 {
-	if (argc < 3)
-	{
-		ROS_ERROR("Usage: point_cloud_viewer [opacity] [map_filename_1] [map_filename_2] [...]");
-		return -1;
-	}
-	float opacity = atof(argv[1]);
+    if (argc < 3)
+    {
+        ROS_ERROR("Usage: point_cloud_viewer [opacity] [map_filename_1] [map_filename_2] [...]");
+        return -1;
+    }
+    float opacity = atof(argv[1]);
 
-	boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer("3D Viewer"));
-	viewer->setBackgroundColor(0,0,0);
-	viewer->registerPointPickingCallback (pointPickingOccurred);
+    boost::shared_ptr<pcl::visualization::PCLVisualizer> viewer (new pcl::visualization::PCLVisualizer("3D Viewer"));
+    viewer->setBackgroundColor(0,0,0);
+    viewer->registerPointPickingCallback (pointPickingOccurred);
 
-	for (int i = 2; i < argc; ++i)
-	{
-		pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
-		pcl::io::loadPCDFile<PointT>(argv[i], *cloud);
+    for (int i = 2; i < argc; ++i)
+    {
+        pcl::PointCloud<PointT>::Ptr cloud (new pcl::PointCloud<PointT>);
+        pcl::io::loadPCDFile<PointT>(argv[i], *cloud);
 
-		*map_cloud_ptr += *cloud;
-	}
+        *map_cloud_ptr += *cloud;
+    }
 
-	pcl::visualization::PointCloudColorHandlerGenericField<PointT> intensity(map_cloud_ptr, "intensity");
-	viewer->addPointCloud<PointT> (map_cloud_ptr, intensity, "map_cloud");
-	viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "map_cloud");
-	viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_OPACITY,opacity, "map_cloud");
+    pcl::visualization::PointCloudColorHandlerGenericField<PointT> intensity(map_cloud_ptr, "intensity");
+    viewer->addPointCloud<PointT> (map_cloud_ptr, intensity, "map_cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "map_cloud");
+    viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_OPACITY,opacity, "map_cloud");
 
-	while(!viewer->wasStopped())
-	{
-		viewer->spinOnce(100);
-		boost::this_thread::sleep (boost::posix_time::microseconds (100000));
-	}
+    while(!viewer->wasStopped())
+    {
+        viewer->spinOnce(100);
+        boost::this_thread::sleep (boost::posix_time::microseconds (100000));
+    }
 
-	return 0;
+    return 0;
 }
