313a93b026e9f986deffa4a954f6d4797a7eaacb
455936 <Dennis.Lu@itri.org.tw>
2018-11-05 22:44:30 +0800

    point_cloud_to_2d: uses string instead of char[] for io

--- src/point_cloud_to_2d/src/point_cloud_to_2d.cpp
+++ src/point_cloud_to_2d/src/point_cloud_to_2d.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 
 #include <pcl/io/pcd_io.h>
 
@@ -10,13 +11,13 @@ typedef pcl::PointCloud<PointT>::Ptr PointCloudTPtr;
 
 int main(int argc, char** argv)
 {
+
     if (argc < 2)
     {
         ROS_ERROR("Usage: point_cloud_to_2d [pcd_filename]");
         return -1;
     }
-    char map_filename [512];
-    strcpy(map_filename, argv[1]);
+    std::string map_filename = argv[1];
 
     PointCloudTPtr map_cloud_ptr (new PointCloudT);
     pcl::io::loadPCDFile<PointT> (map_filename, *map_cloud_ptr);
@@ -24,8 +25,7 @@ int main(int argc, char** argv)
     for (size_t i = 0; i < map_cloud_ptr->points.size(); ++i)
         map_cloud_ptr->points[i].z = 0;
 
-    char new_map_filename [512];
-    sprintf(new_map_filename, "%s_2d", map_filename);
+    std::string new_map_filename = map_filename + "_2d";
     pcl::io::savePCDFileBinary(new_map_filename, *map_cloud_ptr);
 
     return 0;
