8300f1e41cb91667450bccab93260ffe0c1f7fac
455936 <Dennis.Lu@itri.org.tw>
2018-11-05 22:53:50 +0800

    point_cloud_to_2d: employed boost::program_options

--- src/point_cloud_to_2d/src/point_cloud_to_2d.cpp
+++ src/point_cloud_to_2d/src/point_cloud_to_2d.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 
+#include <boost/program_options.hpp>
+
 #include <pcl/io/pcd_io.h>
 
 #include <ros/ros.h>
@@ -8,16 +10,37 @@
 typedef pcl::PointXYZI PointT;
 typedef pcl::PointCloud<PointT> PointCloudT;
 typedef pcl::PointCloud<PointT>::Ptr PointCloudTPtr;
+namespace boost_po = boost::program_options;
+
+std::string map_filename;
 
 int main(int argc, char** argv)
 {
+    boost_po::options_description descriptions{"Allowed options"};
+    descriptions.add_options()
+        ("help", "produce help messages")
+        ("map-filename", boost_po::value<std::string>(), "pcd filename");
 
-    if (argc < 2)
+    boost_po::variables_map var_map;
+    boost_po::store(boost_po::parse_command_line(argc, argv, descriptions), var_map);
+    boost_po::notify(var_map);
+
+    if(var_map.count("help"))
     {
-        ROS_ERROR("Usage: point_cloud_to_2d [pcd_filename]");
+        ROS_INFO_STREAM(std::endl << descriptions);
+        return 1;
+    }
+
+    if(var_map.count("map-filename"))
+    {
+        map_filename = var_map["map-filename"].as<std::string>();
+        ROS_INFO_STREAM("Reading pcd file: " << map_filename);
+    }
+    else
+    {
+        ROS_ERROR("No map file given, exiting");
         return -1;
     }
-    std::string map_filename = argv[1];
 
     PointCloudTPtr map_cloud_ptr (new PointCloudT);
     pcl::io::loadPCDFile<PointT> (map_filename, *map_cloud_ptr);
