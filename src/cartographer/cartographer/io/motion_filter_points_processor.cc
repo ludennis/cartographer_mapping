64689cd6b63b964c2c6efe2d20c0b802fa117919
455936 <Dennis.Lu@itri.org.tw>
2019-01-24 14:53:48 +0800

    motion_filter: added motion filter points processor

---
+++ src/cartographer/cartographer/io/motion_filter_points_processor.cc
@@ -0,0 +1,62 @@
+#include "cartographer/io/motion_filter_points_processor.h"
+
+#include "cartographer/common/lua_parameter_dictionary.h"
+#include "cartographer/common/make_unique.h"
+#include "cartographer/common/time.h"
+#include "cartographer/io/points_batch.h"
+
+namespace cartographer{
+namespace io{
+
+std::unique_ptr<MotionFilterPointsProcessor>
+MotionFilterPointsProcessor::FromDictionary(
+    common::LuaParameterDictionary* dictionary, PointsProcessor* next) {
+  return common::make_unique<MotionFilterPointsProcessor> (
+      dictionary->GetDouble("filter_speed_kmph"),
+      dictionary->GetDouble("filter_distance"),
+      next);
+}
+
+MotionFilterPointsProcessor::MotionFilterPointsProcessor(
+    const double filter_speed_kmph, const double filter_distance,
+    PointsProcessor* next)
+    : filter_speed_kmph_(filter_speed_kmph),
+      filter_distance_(filter_distance),
+      next_(next) {}
+
+
+void MotionFilterPointsProcessor::Process(
+    std::unique_ptr<PointsBatch> batch) {
+  if (batch->index >= 1) {
+    LOG(INFO) << "Previous PointsBatch Timestamp: " << batch->prev_start_time;
+    LOG(INFO) << "PointsBatch Timestamp: " << batch->start_time;
+    LOG(INFO) << "Previous PointsBatch origin: (" << batch->prev_origin[0]
+              << ", " << batch->prev_origin[1] << ", " << batch->prev_origin[2]
+              << ")";
+    LOG(INFO) << "Current PointsBatch origin: (" << batch->origin[0] << ", "
+              << batch->origin[1] << ", " << batch->origin[2] << ")";
+
+    double delta_distance = sqrt( pow(batch->prev_origin[0] - batch->origin[0], 2) +
+                           pow(batch->prev_origin[1] - batch->origin[1], 2) +
+                           pow(batch->prev_origin[2] - batch->origin[2], 2));
+    double delta_seconds = common::ToSeconds(batch->start_time - batch->prev_start_time);
+
+    double speed_mps = delta_distance / delta_seconds;
+    double speed_kmph = speed_mps / 1000. * 60. * 60.;
+
+    LOG(INFO) << "delta_distance: " << delta_distance << "m, delta_seconds: "
+              << delta_seconds << "seconds, speed_mps: " << speed_mps
+              << "m/s, speed_kmph: " << speed_kmph << "km/h";
+
+    if (delta_distance >= filter_distance_ && speed_kmph >= filter_speed_kmph_) {
+      next_->Process(std::move(batch));
+    }
+  }
+}
+
+PointsProcessor::FlushResult MotionFilterPointsProcessor::Flush() {
+  return next_->Flush();
+}
+
+} // namespace io
+} // namespace cartographer
