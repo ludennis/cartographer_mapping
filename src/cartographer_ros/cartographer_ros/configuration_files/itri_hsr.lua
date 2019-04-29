58785660ee34966433c2542a2207a859cd4e9e08
Dennis Lu <dennis.lu@itri.org.tw>
2019-04-29 14:14:45 +0800

    CTGR: updated configuration files

--- src/cartographer_ros/cartographer_ros/configuration_files/itri_hsr.lua
+++ src/cartographer_ros/cartographer_ros/configuration_files/itri_hsr.lua
@@ -48,56 +48,38 @@ MAP_BUILDER.num_background_threads = 12
 TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
 
 -- set these let ceres scan matcher to not trust the priors
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 5
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 5
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 1e-2
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1
 
 -- enlarge size of scan used for constructing each submap
-TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 5e1
+TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 50.
+
+-- set voxel grid of each rangefinder to be 10 cm per grid
+TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.1
 
 -- set resolution of each scan to be 10 cm per grid
-TRAJECTORY_BUILDER_3D.submaps.high_resolution = 2e-1
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.2
 
 -- set the range of rangefinder to be used in scan matching
-TRAJECTORY_BUILDER_3D.min_range = 5
+TRAJECTORY_BUILDER_3D.min_range = 1
+TRAJECTORY_BUILDER_3D.max_range = 100.
 
 -- range data will only be added to submap if greater than max_distance_meters
-TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 1./18.
+TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 1.0
 
 -- turns off residual histogram
 POSE_GRAPH.log_residual_histograms = false
 
 -- other configurations for global slam
-POSE_GRAPH.global_constraint_search_after_n_seconds = 1e1
+POSE_GRAPH.optimize_every_n_nodes = 320
+POSE_GRAPH.global_sampling_ratio = 0.003
+POSE_GRAPH.global_constraint_search_after_n_seconds = 10
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
 POSE_GRAPH.optimization_problem.log_solver_summary = true
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 5e1
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 50
 
--- settings for gps to cause no z-axis misalignment
-POSE_GRAPH.optimization_problem.fixed_frame_pose_translation_weight = 1e1
+-- settings for gps
+POSE_GRAPH.optimization_problem.fixed_frame_pose_translation_weight = 100
 POSE_GRAPH.optimization_problem.fixed_frame_pose_rotation_weight = 0
-
--- settings to find better pose graph solution
-POSE_GRAPH.optimize_every_n_nodes = 5e1
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 4e1
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_z_search_window = 2
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.angular_search_window = math.rad(5)
-POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.min_low_resolution_score = 0.
-POSE_GRAPH.constraint_builder.min_score = 5e-1
-TRAJECTORY_BUILDER_3D.submaps.num_range_data = 25
-POSE_GRAPH.constraint_builder.sampling_ratio = 5e-1
-
--- not trust imu rotation
-POSE_GRAPH.optimization_problem.rotation_weight = 1e-2
-
--- -- setting to cause closeby trajectory to find constraints along z-axis
--- POSE_GRAPH.constraint_builder.loop_closure_translation_weight = 1
--- POSE_GRAPH.constraint_builder.loop_closure_rotation_weight = 3e4
--- POSE_GRAPH.constraint_builder.log_matches = true
--- POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.translation_weight = 1
--- POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.rotation_weight = 1e3
--- POSE_GRAPH.global_sampling_ratio = 3e-1
-
-
-
 
 return options
