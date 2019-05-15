a570065ee278175b3fc7c42bf9f128be1b8c0a43
Dennis Lu <dennis.lu@itri.org.tw>
2019-05-15 12:56:47 +0800

    itri_campus: updated parameters for better mapping result

--- src/cartographer_ros/cartographer_ros/configuration_files/itri_campus.lua
+++ src/cartographer_ros/cartographer_ros/configuration_files/itri_campus.lua
@@ -1,17 +1,3 @@
--- Copyright 2016 The Cartographer Authors
---
--- Licensed under the Apache License, Version 2.0 (the "License");
--- you may not use this file except in compliance with the License.
--- You may obtain a copy of the License at
---
---      http://www.apache.org/licenses/LICENSE-2.0
---
--- Unless required by applicable law or agreed to in writing, software
--- distributed under the License is distributed on an "AS IS" BASIS,
--- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
--- See the License for the specific language governing permissions and
--- limitations under the License.
-
 include "map_builder.lua"
 include "trajectory_builder.lua"
 
@@ -39,47 +25,76 @@ options = {
   trajectory_publish_period_sec = 30e-3,
   rangefinder_sampling_ratio = 1.,
   odometry_sampling_ratio = 1.,
-  imu_sampling_ratio = 1.0,
+  imu_sampling_ratio = 1.,
 }
 
 MAP_BUILDER.use_trajectory_builder_3d = true
-MAP_BUILDER.num_background_threads = 12
+MAP_BUILDER.num_background_threads = 15
 
-TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 1
+-- no point of traying to SLAM over points on the vehicle 2019-01-18
+TRAJECTORY_BUILDER_3D.min_range = 2.5
+TRAJECTORY_BUILDER_3D.max_range = 150
 
+-- 2019-01-18 commented
 -- set these let ceres scan matcher to not trust the priors
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 1e-2
-TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1
+-- TRAJECTORY_BUILDER_3D.ceres_scan_matcher.translation_weight = 1e-2
+-- TRAJECTORY_BUILDER_3D.ceres_scan_matcher.rotation_weight = 1
 
--- enlarge size of scan used for constructing each submap
-TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 50.
+-- 2019-01-18
+-- Use more points for SLAMing and adapt a bit for the ranges of a car
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.max_length = 5.
+TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter.min_num_points = 250.
+TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.max_length = 8.
+TRAJECTORY_BUILDER_3D.low_resolution_adaptive_voxel_filter.min_num_points = 400.
 
--- set voxel grid of each rangefinder to be 10 cm per grid
-TRAJECTORY_BUILDER_3D.voxel_filter_size = 0.1
+-- 2019-01-18
+-- the submaps felt prety big - since the car moves faster, we want them to be
+-- slightly smaller. Slamming at 10cm can be aggressive for cars and for the quality
+-- of the rangefinder. Increased 'high_resolution', '*num_iterations' in the
+-- various optimization problems also trades performance/quality
+-- TRAJECTORY_BUILDER_3D.submaps.num_range_data = 60
+TRAJECTORY_BUILDER_3D.submaps.high_resolution = .25
+TRAJECTORY_BUILDER_3D.submaps.low_resolution = .60
+TRAJECTORY_BUILDER_3D.ceres_scan_matcher.ceres_solver_options.max_num_iterations = 50
 
--- set resolution of each scan to be 10 cm per grid
-TRAJECTORY_BUILDER_3D.submaps.high_resolution = 0.2
+-- 2019-01-18
+-- Trying to loop close too often will cost CPU and not buy you a lot. There is
+-- little point in trying more than once per submap
+-- POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 200
+-- POSE_GRAPH.constraint_builder.min_score = 0.5
 
--- set the range of rangefinder to be used in scan matching
-TRAJECTORY_BUILDER_3D.min_range = 1
-TRAJECTORY_BUILDER_3D.max_range = 100.
 
--- range data will only be added to submap if greater than max_distance_meters
-TRAJECTORY_BUILDER_3D.motion_filter.max_distance_meters = 1.0
+-- 2019-01-18
+-- Crazy search window to force loop closure to work. All other changes are probably not needed
+POSE_GRAPH.constraint_builder.max_constraint_distance = 2.5e2
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_xy_search_window = 2.5e2
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.linear_z_search_window = 3e1
+POSE_GRAPH.constraint_builder.fast_correlative_scan_matcher_3d.angular_search_window = math.rad(2e1)
+POSE_GRAPH.constraint_builder.ceres_scan_matcher_3d.ceres_solver_options.max_num_iterations = 5e1
 
--- turns off residual histogram
-POSE_GRAPH.log_residual_histograms = false
+-- increases weighting for gps 2019-03-13
+-- suggestion: increase it more to prevent zhanglang bridge from squeezing
+POSE_GRAPH.optimization_problem.fixed_frame_pose_translation_weight = 2e1
+-- POSE_GRAPH.optimization_problem.fixed_frame_pose_rotation_weight = 0
 
--- other configurations for global slam
-POSE_GRAPH.optimize_every_n_nodes = 320
-POSE_GRAPH.global_sampling_ratio = 0.003
-POSE_GRAPH.global_constraint_search_after_n_seconds = 10
+-- increase submap size for mapping on zhanglang bridge 2019-03-13
+TRAJECTORY_BUILDER_3D.submaps.high_resolution_max_range = 7e1
+
+
+-- changes made to find loop closure
+-- increases min score to get better loop closure at zhanglang bridge 2019-03-13
+POSE_GRAPH.constraint_builder.min_score = 0.3
+-- increases sampling ratio to allow finding loop closure 2019-03-15
+POSE_GRAPH.constraint_builder.sampling_ratio = 0.03
+-- decrease submap size for more flexibility 2019-03-14
+TRAJECTORY_BUILDER_3D.submaps.num_range_data = 1e2
+POSE_GRAPH.optimize_every_n_nodes = 1e2
 POSE_GRAPH.optimization_problem.huber_scale = 1e2
-POSE_GRAPH.optimization_problem.log_solver_summary = true
-POSE_GRAPH.optimization_problem.ceres_solver_options.max_num_iterations = 50
 
--- settings for gps
-POSE_GRAPH.optimization_problem.fixed_frame_pose_translation_weight = 100
-POSE_GRAPH.optimization_problem.fixed_frame_pose_rotation_weight = 0
+POSE_GRAPH.max_num_final_iterations = 2e3
+
+-- added for using velodyne packets 2019-03-24
+TRAJECTORY_BUILDER_3D.num_accumulated_range_data = 626;
 
 return options
