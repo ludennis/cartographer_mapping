f83437063c6eae24f3d7e7d5d6acfad4c1cbbabe
Dennis Lu <dennis.lu@itri.org.tw>
2019-05-15 12:55:09 +0800

    itri_writes_to_xyzi: updated motion filter

--- src/cartographer_ros/cartographer_ros/configuration_files/itri_writes_to_xyzi.lua
+++ src/cartographer_ros/cartographer_ros/configuration_files/itri_writes_to_xyzi.lua
@@ -15,22 +15,21 @@
 include "transform.lua"
 
 options = {
+  project_to_nav_sat = true,
+  latitude_reference = 24.775084704,
+  longitude_reference = 121.045888961,
+  altitude_reference = 146.593,
   tracking_frame = "base_imu",
   pipeline = {
     {
         action = "min_max_range_filter",
-        min_range = 1.,
-        max_range = 200.,
+        min_range = 2.0,
+        max_range = 300.,
     },
     {
         action = "motion_filter",
-        filter_speed_kmph = 1.0,
-        filter_distance = 0.1,
-    },
-    {
-        action = "motion_filter",
-        filter_speed_kmph = 0.1,
-        filter_distance = 0.01,
+        filter_speed_kmph = 1.5,
+        filter_distance = 0.0,
     },
     {
         action = "intensity_to_color",
@@ -38,7 +37,6 @@ options = {
         max_intensity = 256.,
     },
     {
-        --action = "grid_map_write_pcd",
         action = "write_pcd",
         filename = "full.pcd",
     },
