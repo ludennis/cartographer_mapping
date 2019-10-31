5a6d15a6358dc92cbc5c9876ff04fbdc1d5e186e
Dennis Lu <dennis.lu@itri.org.tw>
2019-10-31 15:22:37 +0800

    create_submaps: refactored fucntions "WriteToJsonFile()" "CreateSubmapsWithConfig()" & "GenerateConfig()"

--- src/create_submaps/src/create_submaps.cpp
+++ src/create_submaps/src/create_submaps.cpp
@@ -37,6 +37,203 @@ std::string strGpsReferenceArray;
 std::vector<std::string> gpsReferenceArray;
 std::string submapsDirectory;
 
+void WriteToJsonFile(
+  const int numSubmaps, const int totalPoints, const Json::Value& arrayObj)
+{
+  Json::Value rootJsonValue;
+  rootJsonValue["numSubmaps"] = numSubmaps;
+  rootJsonValue["totalPoints"] = totalPoints;
+  rootJsonValue["submaps"] = arrayObj;
+  Json::Value gpsObj;
+  gpsObj["latitude"] = gpsReferenceArray[0];
+  gpsObj["longitude"] = gpsReferenceArray[1];
+  gpsObj["altitude"] = gpsReferenceArray[2];
+  rootJsonValue["gps_reference"] = gpsObj;
+  Json::StreamWriterBuilder builder;
+  builder["commentStyle"] = "None";
+  builder["indentation"] = "    ";
+  std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
+  std::string outFilename = submapsDirectory + "/submaps_config.json";
+  std::cout << "Writing to file: " << outFilename << std::endl;
+  std::ofstream outputFileStream(outFilename);
+  writer->write(rootJsonValue, &outputFileStream);
+}
+
+void CreateSubmapsWithConfig(
+  const std::string& fileFormat, const int submapSize, const std::string& mapFileName)
+{
+  PointCloudTPtr inputCloud (new PointCloudT);
+  if (pcl::io::loadPCDFile(mapFilename, *inputCloud) == -1)
+  {
+    ROS_ERROR_STREAM("Couldn't read file: " << mapFilename);
+    return;
+  }
+
+  PointT mapMinPoint, mapMaxPoint;
+  pcl::getMinMax3D (*inputCloud, mapMinPoint, mapMaxPoint);
+  ROS_INFO_STREAM("" << "Max x: " << mapMaxPoint.x);
+  ROS_INFO_STREAM("" << "Max y: " << mapMaxPoint.y);
+  ROS_INFO_STREAM("" << "Max z: " << mapMaxPoint.z);
+  ROS_INFO_STREAM("" << "Min x: " << mapMinPoint.x);
+  ROS_INFO_STREAM("" << "Min y: " << mapMinPoint.y);
+  ROS_INFO_STREAM("" << "Min z: " << mapMinPoint.z);
+
+  const int xGridSize = (int) ceil((mapMaxPoint.x - mapMinPoint.x) / submapSize);
+  const int yGridSize = (int) ceil((mapMaxPoint.y - mapMinPoint.y) / submapSize);
+
+  PointCloudArray2D submaps(boost::extents[xGridSize][yGridSize]);
+
+  for (unsigned int index = 0; index < inputCloud->size(); ++index)
+  {
+    const int xGrid =
+      (int) floor( (inputCloud->points[index].x - mapMinPoint.x) / submapSize);
+    const int yGrid =
+      (int) floor( (inputCloud->points[index].y - mapMinPoint.y) / submapSize);
+    submaps[xGrid][yGrid].points.push_back(inputCloud->points[index]);
+  }
+
+  std::string removedText = ".pcd";
+  std::size_t textIndex = mapFilename.find(removedText);
+  if (textIndex != std::string::npos)
+  {
+    mapFilename.erase(textIndex, removedText.length());
+  }
+
+  std::string outFileDirectory = mapFilename + "_submaps/";
+  boost::filesystem::create_directories(outFileDirectory);
+
+  for (int x = 0; x < xGridSize; ++x)
+  {
+    for (int y = 0; y < yGridSize; ++y)
+    {
+      if (submaps[x][y].size() > 0)
+      {
+        submaps[x][y].width = (int) submaps[x][y].points.size();
+        submaps[x][y].height = 1;
+        ROS_INFO("submaps[%d][%d]: points.size() = %ld, width = %d, "
+          "height = %d, is_dense = %d", x, y, submaps[x][y].points.size(),
+          submaps[x][y].width, submaps[x][y].height, submaps[x][y].is_dense);
+        std::string submapFilename =
+          "submap_" + std::to_string(x) + "_" + std::to_string(y) + ".pcd";
+
+        if (fileFormat.compare("ascii") == 0)
+        {
+          if (pcl::io::savePCDFileASCII(outFileDirectory + submapFilename, submaps[x][y]) == -1)
+            ROS_ERROR_STREAM("Failed saving: " << submapFilename);
+        }
+        else
+        {
+          if (pcl::io::savePCDFileBinary(outFileDirectory + submapFilename, submaps[x][y]) == -1)
+            ROS_ERROR_STREAM("Failed saving: " << submapFilename);
+        }
+      }
+    }
+  }
+
+  // Get submaps_config.json
+  Json::Value arrayObj;
+  int numSubmaps = 0;
+  int totalPoints = 0;
+
+  for (auto & inputFile : fs::directory_iterator(outFileDirectory))
+  {
+    std::cout << "processing " << inputFile << std::endl;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    Json::Value tmp_value;
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (fs::path(inputFile).string(), *cloud) == -1)
+    {
+      PCL_ERROR ("Couldn't read file %s\n", fs::path(inputFile).string().c_str());
+    }
+    else
+    {
+      std::cout << "Loaded "
+                << cloud->width * cloud->height
+                << " data points from test_pcd.pcd with the following fields: "
+                << std::endl;
+
+      std::vector<float> ptX;
+      std::vector<float> ptY;
+      for (size_t i = 0; i < cloud->points.size(); ++i)
+      {
+        ptX.push_back(cloud->points[i].x);
+        ptY.push_back(cloud->points[i].y);
+      }
+
+      const float centerX = 0.5f * (
+        *std::max_element(ptX.begin(), ptX.end()) +
+        *std::min_element(ptX.begin(), ptX.end()));
+
+      const float centerY = 0.5f * (
+        *std::max_element(ptY.begin(), ptY.end()) +
+        *std::min_element(ptY.begin(), ptY.end()));
+
+      tmp_value["numPoints"] = Json::Value(
+        static_cast<int>(cloud->points.size()));
+      tmp_value["fileName"] = fs::path(inputFile).filename().string().c_str();
+      tmp_value["centerX"] = Json::Value(centerX);
+      tmp_value["centerY"] = Json::Value(centerY);
+      arrayObj.append(tmp_value);
+      numSubmaps ++;
+      totalPoints += cloud->points.size();
+    }
+  }
+  WriteToJsonFile(numSubmaps, totalPoints, arrayObj);
+}
+
+void GenerateConfig(const std::string& submapsDirectory)
+{
+  Json::Value arrayObj;
+  int numSubmaps = 0;
+  int totalPoints = 0;
+  for (auto & inputFile : fs::directory_iterator(submapsDirectory))
+  {
+    std::cout << "processing " << inputFile << std::endl;
+    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+
+    Json::Value tmpValue;
+
+    if (pcl::io::loadPCDFile<pcl::PointXYZ> (fs::path(inputFile).string(), *cloud) == -1)
+    {
+      PCL_ERROR ("Couldn't read file %s\n", fs::path(inputFile).string().c_str());
+    }
+    else
+    {
+      std::cout << "Loaded "
+                << cloud->width * cloud->height
+                << " data points from test_pcd.pcd with the following fields: "
+                << std::endl;
+
+      std::vector<float> ptX;
+      std::vector<float> ptY;
+      for (size_t i = 0; i < cloud->points.size(); ++i)
+      {
+        ptX.push_back(cloud->points[i].x);
+        ptY.push_back(cloud->points[i].y);
+      }
+
+      const float centerX = 0.5f * (
+        *std::max_element(ptX.begin(), ptX.end()) +
+        *std::min_element(ptX.begin(), ptX.end()));
+
+      const float centerY = 0.5f * (
+        *std::max_element(ptY.begin(), ptY.end()) +
+        *std::min_element(ptY.begin(), ptY.end()));
+
+      tmpValue["numPoints"] = Json::Value(
+        static_cast<int>(cloud->points.size()));
+      tmpValue["fileName"] = fs::path(inputFile).filename().string().c_str();
+      tmpValue["centerX"] = Json::Value(centerX);
+      tmpValue["centerY"] = Json::Value(centerY);
+      arrayObj.append(tmpValue);
+      numSubmaps++;
+      totalPoints += cloud->points.size();
+    }
+  }
+  WriteToJsonFile(numSubmaps, totalPoints, arrayObj);
+}
+
 int main (int argc, char** argv)
 {
   boost_po::options_description description{"Allowed options"};
@@ -129,211 +326,14 @@ int main (int argc, char** argv)
     mapFilename = var_map["map-filename"].as<std::string>();
     ROS_INFO_STREAM("Loading map file: " << mapFilename);
 
-    PointCloudTPtr inputCloud (new PointCloudT);
-    if (pcl::io::loadPCDFile(mapFilename, *inputCloud) == -1)
-    {
-      ROS_ERROR_STREAM("Couldn't read file: " << mapFilename);
-      return -1;
-    }
-
-    PointT map_min_point, map_max_point;
-    pcl::getMinMax3D (*inputCloud, map_min_point, map_max_point);
-    ROS_INFO_STREAM("" << "Max x: " << map_max_point.x);
-    ROS_INFO_STREAM("" << "Max y: " << map_max_point.y);
-    ROS_INFO_STREAM("" << "Max z: " << map_max_point.z);
-    ROS_INFO_STREAM("" << "Min x: " << map_min_point.x);
-    ROS_INFO_STREAM("" << "Min y: " << map_min_point.y);
-    ROS_INFO_STREAM("" << "Min z: " << map_min_point.z);
-
-    const int x_grid_size = (int) ceil((map_max_point.x - map_min_point.x) / submapSize);
-    const int y_grid_size = (int) ceil((map_max_point.y - map_min_point.y) / submapSize);
-
-    PointCloudArray2D submaps(boost::extents[x_grid_size][y_grid_size]);
-
-    for (unsigned int index = 0; index < inputCloud->size(); ++index)
-    {
-      const int x_grid =
-        (int) floor( (inputCloud->points[index].x - map_min_point.x) / submapSize);
-      const int y_grid =
-        (int) floor( (inputCloud->points[index].y - map_min_point.y) / submapSize);
-      submaps[x_grid][y_grid].points.push_back(inputCloud->points[index]);
-    }
-
-    std::string removed_text = ".pcd";
-    std::size_t text_index = mapFilename.find(removed_text);
-    if (text_index != std::string::npos)
-    {
-      mapFilename.erase(text_index, removed_text.length());
-    }
-
-    std::string out_file_directory = mapFilename + "_submaps/";
-    boost::filesystem::create_directories(out_file_directory);
-
-    for (int x = 0; x < x_grid_size; ++x)
-    {
-      for (int y = 0; y < y_grid_size; ++y)
-      {
-        if (submaps[x][y].size() > 0)
-        {
-          submaps[x][y].width = (int) submaps[x][y].points.size();
-          submaps[x][y].height = 1;
-          ROS_INFO("submaps[%d][%d]: points.size() = %ld, width = %d, "
-            "height = %d, is_dense = %d", x, y, submaps[x][y].points.size(),
-            submaps[x][y].width, submaps[x][y].height, submaps[x][y].is_dense);
-          std::string submapFilename =
-            "submap_" + std::to_string(x) + "_" + std::to_string(y) + ".pcd";
-
-          if (fileFormat.compare("ascii") == 0)
-          {
-            if (pcl::io::savePCDFileASCII(out_file_directory + submapFilename, submaps[x][y]) == -1)
-              ROS_ERROR_STREAM("Failed saving: " << submapFilename);
-          }
-          else
-          {
-            if (pcl::io::savePCDFileBinary(out_file_directory + submapFilename, submaps[x][y]) == -1)
-              ROS_ERROR_STREAM("Failed saving: " << submapFilename);
-          }
-        }
-      }
-    }
-
-    // Get submaps_config.json
-    Json::Value array_obj;
-    int num_submaps = 0;
-    int total_points = 0;
-
-    for (auto & input_file : fs::directory_iterator(out_file_directory))
-    {
-      std::cout << "processing " << input_file << std::endl;
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-
-      Json::Value tmp_value;
-
-      if (pcl::io::loadPCDFile<pcl::PointXYZ> (fs::path(input_file).string(), *cloud) == -1)
-      {
-        PCL_ERROR ("Couldn't read file %s\n", fs::path(input_file).string().c_str());
-      }
-      else
-      {
-        std::cout << "Loaded "
-                  << cloud->width * cloud->height
-                  << " data points from test_pcd.pcd with the following fields: "
-                  << std::endl;
-
-        std::vector<float> pt_x;
-        std::vector<float> pt_y;
-        for (size_t i = 0; i < cloud->points.size(); ++i)
-        {
-          pt_x.push_back(cloud->points[i].x);
-          pt_y.push_back(cloud->points[i].y);
-        }
-
-        const float center_x = 0.5f * (
-          *std::max_element(pt_x.begin(), pt_x.end()) +
-          *std::min_element(pt_x.begin(), pt_x.end()));
-
-        const float center_y = 0.5f * (
-          *std::max_element(pt_y.begin(), pt_y.end()) +
-          *std::min_element(pt_y.begin(), pt_y.end()));
-
-        tmp_value["num_points"] = Json::Value(
-          static_cast<int>(cloud->points.size()));
-        tmp_value["file_name"] = fs::path(input_file).filename().string().c_str();
-        tmp_value["center_x"] = Json::Value(center_x);
-        tmp_value["center_y"] = Json::Value(center_y);
-        array_obj.append(tmp_value);
-        num_submaps ++;
-        total_points += cloud->points.size();
-      }
-    }
-
-    Json::Value root_json_value;
-    root_json_value["num_submaps"] = num_submaps;
-    root_json_value["total_points"] = total_points;
-    root_json_value["submaps"] = array_obj;
-    Json::Value gps_obj;
-    gps_obj["latitude"] = gpsReferenceArray[0];
-    gps_obj["longitude"] = gpsReferenceArray[1];
-    gps_obj["altitude"] = gpsReferenceArray[2];
-    root_json_value["gps_reference"] = gps_obj;
-    Json::StreamWriterBuilder builder;
-    builder["commentStyle"] = "None";
-    builder["indentation"] = "    ";
-    std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-    std::cout << "Writing to file: " << out_file_directory + "submaps_config.json" << std::endl;
-    std::ofstream output_file_stream(out_file_directory + "submaps_config.json");
-    writer -> write(root_json_value, &output_file_stream);
+    CreateSubmapsWithConfig(fileFormat, submapSize, mapFilename);
   }
   else if (var_map.count("generate-config"))
   {
     submapsDirectory = var_map["generate-config"].as<std::string>();
     ROS_INFO_STREAM("Generating submaps config file at: " << submapsDirectory);
 
-    Json::Value arrayObj;
-    int numSubmaps = 0;
-    int totalPoints = 0;
-    for (auto & inputFile : fs::directory_iterator(submapsDirectory))
-    {
-      std::cout << "processing " << inputFile << std::endl;
-      pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
-
-      Json::Value tmpValue;
-
-      if (pcl::io::loadPCDFile<pcl::PointXYZ> (fs::path(inputFile).string(), *cloud) == -1)
-      {
-        PCL_ERROR ("Couldn't read file %s\n", fs::path(inputFile).string().c_str());
-      }
-      else
-      {
-        std::cout << "Loaded "
-                  << cloud->width * cloud->height
-                  << " data points from test_pcd.pcd with the following fields: "
-                  << std::endl;
-
-        std::vector<float> ptX;
-        std::vector<float> ptY;
-        for (size_t i = 0; i < cloud->points.size(); ++i)
-        {
-          ptX.push_back(cloud->points[i].x);
-          ptY.push_back(cloud->points[i].y);
-        }
-
-        const float centerX = 0.5f * (
-          *std::max_element(ptX.begin(), ptX.end()) +
-          *std::min_element(ptX.begin(), ptX.end()));
-
-        const float centerY = 0.5f * (
-          *std::max_element(ptY.begin(), ptY.end()) +
-          *std::min_element(ptY.begin(), ptY.end()));
-
-        tmpValue["num_points"] = Json::Value(
-          static_cast<int>(cloud->points.size()));
-        tmpValue["file_name"] = fs::path(inputFile).filename().string().c_str();
-        tmpValue["center_x"] = Json::Value(centerX);
-        tmpValue["center_y"] = Json::Value(centerY);
-        arrayObj.append(tmpValue);
-        numSubmaps ++;
-        totalPoints += cloud->points.size();
-      }
-    }
-
-    Json::Value rootJsonValue;
-    rootJsonValue["num_submaps"] = numSubmaps;
-    rootJsonValue["total_points"] = totalPoints;
-    rootJsonValue["submaps"] = arrayObj;
-    Json::Value gpsObj;
-    gpsObj["latitude"] = gpsReferenceArray[0];
-    gpsObj["longitude"] = gpsReferenceArray[1];
-    gpsObj["altitude"] = gpsReferenceArray[2];
-    rootJsonValue["gps_reference"] = gpsObj;
-    Json::StreamWriterBuilder builder;
-    builder["commentStyle"] = "None";
-    builder["indentation"] = "    ";
-    std::unique_ptr<Json::StreamWriter> writer(builder.newStreamWriter());
-    std::string outFilename = submapsDirectory + "/submaps_config.json";
-    std::cout << "Writing to file: " << outFilename << std::endl;
-    std::ofstream outputFileStream(outFilename);
-    writer -> write(rootJsonValue, &outputFileStream);
+    GenerateConfig(submapsDirectory);
   }
 
   return 0;
