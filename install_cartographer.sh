dc89efccd7b9a6867f072dcd874d1c441116fd66
Dennis Lu <ludennis0606@gmail.com>
2021-05-25 16:03:43 +0800

    install_cartographer.sh: added install steps for noetic install

--- install_cartographer.sh
+++ install_cartographer.sh
@@ -1,12 +1,22 @@
 rm -rf build_isolated/ devel_isolated/ install_isolated/ src/ protobuf/
 
 sudo apt-get update
-sudo apt-get install -y python-wstool python-rosdep ninja-build
+
+if [ "$(rosversion -d)" == "noetic" ]; then
+  sudo apt-get install -y python3-wstool python3-rosdep ninja-build
+else
+  sudo apt-get install -y python-wstool python-rosdep ninja-build
+fi
 
 wstool init src
 
 wstool merge -t src https://raw.githubusercontent.com/googlecartographer/cartographer_ros/master/cartographer_ros.rosinstall
 wstool update -t src
+
+if [ "$(rosversion -d)" == "noetic" ]; then
+  sudo apt-get install stow
+  src/cartographer/scripts/install_abseil.sh
+fi
 
 src/cartographer/scripts/install_proto3.sh
 
